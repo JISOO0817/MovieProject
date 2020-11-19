964bbefad64d900d2aa4801bf5ab647660c93fdc
JISOO0817 <dkadnsk@naver.com>
2020-11-19 19:22:41 +0900

    Initial commit

---
+++ app/src/main/java/com/example/movieproject/utils/Credentials.java
@@ -0,0 +1,14 @@
+package com.example.movieproject.utils;
+
+public class Credentials {
+
+
+    public static final String BASE_URL = "https://api.themoviedb.org";
+
+    public static final String API_KEY = "fff72fc82622cdebdad5616b557c33dd";
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/movieproject/utils/MovieApi.java
@@ -0,0 +1,4 @@
+package com.example.movieproject.utils;
+
+public interface MovieApi {
+}
